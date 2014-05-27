646f02b82f871ecb5d5bbed191d5d92e2900317a
Alexandros Georgantas <alexgeorg86@gmail.com>
2014-05-27 03:47:21 +0100

    Upload problem three

--- problemThree.rb
+++ problemThree.rb
@@ -1,5 +1,20 @@
-number = 600851475143
+big_number = 600851475143
 
-def is_prime(num)
-	# TODO
+$c = []
+
+def prime_factors(number)
+
+	a = number
+	b = 2
+
+	if(a > 1)
+		while a % b != 0
+			b += 1
+		end
+		$c << b
+		prime_factors(a / b)
+	end
 end
+
+puts prime_factors(big_number)
+print $c.last
